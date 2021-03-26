67ffc480748189baeab2abe61e85159133e1004b
Aidan Christopher <gandamovies@gmail.com>
2021-03-26 01:18:43 -0700

    riot API inplementation

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -3,6 +3,7 @@ const { Store } = require("express-session");
 const database = require("../models/userModel").database;
 const router = express.Router();
 const { ensureAuthenticated, isAdmin } = require("../middleware/checkAuth");
+const request = require('request');
 
 router.get("/", (req, res) => {
   res.redirect('/dashboard');
@@ -20,7 +21,8 @@ router.get("/dashboard", ensureAuthenticated, (req, res) => {
           user: req.user,
           sessions,
           database,
-          img_url: 'https://timesofindia.indiatimes.com/photo/67586673.cms'
+          img_url: 'https://timesofindia.indiatimes.com/photo/67586673.cms',
+          val: [false, null]
         });
       }
     });
@@ -73,7 +75,8 @@ router.get("/admindashboard", ensureAuthenticated, (req, res) => {
           user: req.user,
           sessions,
           database,
-          img_url: req.user.imageurl
+          img_url: req.user.imageurl,
+          val: [false, null]
         });
       }
     });
@@ -100,5 +103,45 @@ router.get("/revokeall", (req, res) => {
   })
   res.redirect("/admin");
 });
+
+router.post("/valorant", (req, res) => {
+  const store = req.sessionStore;
+  store.all((error, sessions) => {
+    if (error) {
+      console.log(error);
+    } else {
+      let acc = req.body.riotid
+      if (acc.length < 2) {
+        res.redirect('/admindashboard')
+      }
+      else {
+        let acc_info = acc.split('#')
+        request('https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' + acc_info[0] + '/' + acc_info[1] + '?api_key=RGAPI-b54f905e-cb77-4c85-b3a9-7e7cb67718fd', function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            if (req.user.role == 'admin') {
+              res.render('admindashboard', {
+                user: req.user,
+                sessions,
+                database,
+                img_url: req.user.imageurl,
+                val: [true, JSON.parse(body)]
+              })
+            };
+            if (req.user.role == 'user') {
+              res.render('dashboard', {
+                user: req.user,
+                sessions,
+                database,
+                img_url: req.user.imageurl,
+                val: [true, JSON.parse(body)]
+              });
+            }
+          }
+        })
+      }
+    }
+  })
+});
+
 
 module.exports = router;
